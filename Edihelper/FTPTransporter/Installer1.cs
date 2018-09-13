8583f8de8c81a44cb63e8fdb3a9a30bd9f3f7811
Maxim <maxkarab@yandex.ru>
2018-09-13 15:43:48 +0400

    Добавлен проект службы. Исправлены ошибки при работе с json файлами настроек. Теперь служба запускается корректно.

---
+++ Edihelper/FTPTransporter/Installer1.cs
@@ -0,0 +1,28 @@
+﻿namespace FTPTransporter
+{
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    using System.Configuration.Install;
+    using FTPGui.BusinessLogicLayer;
+
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public Installer1()
+        {
+            InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.User
+            };
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = SettingsContainer.Settings.ServiceName;
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
